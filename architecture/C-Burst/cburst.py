1ed9ca655969f7c18ee616bd475cb2e43bc9791f
haihai <haihai.self@outlook.com>
2019-11-29 00:39:00 +0800

    update

--- architecture/C-Burst/cburst.py
+++ architecture/C-Burst/cburst.py
@@ -71,8 +71,8 @@ class CBurst:
         temp = temp >> 12
         temp = temp << 12
         result.append(temp)
-        for i in range(1,block_num):
-            temp = temp + 1 << 12
+        for i in range(1, block_num):
+            temp = temp + (1 << 12)
             result.append(temp)
         return result
 
@@ -85,11 +85,12 @@ class LRUOnly(CBurst):
         self.cache = []
         self.hit = 0.0
         self.miss = 0.0
+        self.total = 0.0
 
     def isHit(self, index):  # 判断是否命中
         hit_flag = False
         line = 0
-        for i in range(len(self.cache)):
+        for i in range(len(self.cache) - 1, -1, -1):
             if self.cache[i] == 0:
                 break
             elif index == self.cache[i]:
@@ -107,10 +108,11 @@ class LRUOnly(CBurst):
         # finish_flags = [False for _ in range(len(file_list))]
 
         while True:
+            self.total += 1
             for i in range(len(current_lines)):  # current_lines 保存的是每个文件顶部的行
                 if current_lines[i] == "0":
                     current_lines[i] = file_list[i].readline()
-                if current_lines[i] == "":
+                elif current_lines[i] == "":
                     current_lines.pop(i)
             if len(set(current_lines)) == 0:
                 break
@@ -121,28 +123,29 @@ class LRUOnly(CBurst):
                 if (float(line[0]) - min_time) < 0:
                     min_time = float(line[0])
                     current_line = i
-            print(current_lines[current_line], self.hit, self.miss, len(self.cache))
             line = current_lines[current_line].strip().split(',')
             addr = line[4]  # 获取本次处理的地址
             block_num = int(line[5]) // (1 << 12) + 1  # 本次处理的块数
             block_indexs = super().getBlockTag(addr, block_num)
             hit_flag = [False for _ in range(block_num)]
             for i in range(block_num):  # 命中检测
-                hit_index = self.isHit(block_indexs[i])
-                if hit_index != -1:
+                if block_indexs[i] in self.cache:
                     self.hit += 1
-                    # hit_line = self.cache[hit_index]
-                    hit_line = self.cache.pop(hit_index)
-                    self.cache.append(hit_line)
+                    self.cache.remove(block_indexs[i])
+                    self.cache.append(block_indexs[i])
                     hit_flag[i] = True
+
             for i in range(block_num):  # 缺失处理
                 if not hit_flag[i]:
                     self.miss += 1
                     if len(self.cache) < BLOCK_NUM:
                         self.cache.append(block_indexs[i])
                     else:
-                        line = self.cache.pop(0)
-                        self.cache.append(line)
+                        self.cache.pop(0)
+                        self.cache.append(block_indexs[i])
+            if self.total % 5000 == 0:
+                print(current_lines[current_line], self.hit, self.miss, len(self.cache), self.total, "\naccuracy：",
+                      self.hit / (self.hit + self.miss), time.time())
             current_lines[current_line] = "0"
 
         super().closeFile(file_list)
